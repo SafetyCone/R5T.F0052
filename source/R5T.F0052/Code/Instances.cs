633dd2600b2fd618f60ad5a212579769c6ac9275
davidcoats <david@getsafetycone.com>
2023-05-14 23:44:28 -0700

    Interim changes.

--- source/R5T.F0052/Code/Instances.cs
+++ source/R5T.F0052/Code/Instances.cs
@@ -1,8 +1,5 @@
 using System;
 
-using R5T.F0000;
-using R5T.F0002;
-
 
 namespace R5T.F0052
 {
@@ -10,7 +7,7 @@ namespace R5T.F0052
     {
         public static IDirectoryNames DirectoryNames { get; } = F0052.DirectoryNames.Instance;
         public static IFileExtensions FileExtensions { get; } = F0052.FileExtensions.Instance;
-        public static IFileExtensionOperator FileExtensionOperator { get; } = F0000.FileExtensionOperator.Instance;
+        public static F0000.IFileExtensionOperator FileExtensionOperator { get; } = F0000.FileExtensionOperator.Instance;
         public static IFileNames FileNames { get; } = F0052.FileNames.Instance;
         public static F0002.IPathOperator PathOperator { get; } = F0002.PathOperator.Instance;
         public static IProjectDirectoryNameOperator ProjectDirectoryNameOperator { get; } = F0052.ProjectDirectoryNameOperator.Instance;
